1515b5f1462379456507c438a68e25be2706f582
vannyou_tang <vannyoutang23@gmail.com>
2020-10-23 14:42:49 +0700

    added test case of api of update announcement

--- tests/integration/routes/announcements.test.js
+++ tests/integration/routes/announcements.test.js
@@ -226,4 +226,193 @@ describe('Announcements', () => {
             expect(dt).toHaveProperty('indexPosition', 0);
         });
     });
+
+    describe('PUT /api/v1/announcements/:id', () => {
+        let announcement;
+        let announcementId;
+
+        beforeEach(async () => {
+            announcement = await Announcement.create({
+                title: 'Free Free',
+                description: 'Buy Ticket, Free Popcorn',
+                startedDateTime: '2023-10-10 10:00',
+                endedDateTime: '2023-12-12 10:00'
+            });
+
+            announcementId = announcement._id;
+        });
+
+        const exec = () =>
+            request(server).put(`/api/v1/announcements/${announcementId}`);
+
+        it('should return 404 if object ID of announcement is not valid', async () => {
+            announcementId = 1;
+            const res = await exec().send({});
+
+            expect(res.status).toBe(404);
+        });
+
+        it('should return 404 if object ID of announcement does not exist', async () => {
+            announcementId = mongoose.Types.ObjectId();
+            const res = await exec().send({});
+
+            expect(res.status).toBe(404);
+        });
+
+        it('should return 400 if title is not a string', async () => {
+            const res = await exec().send({ title: true });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if title is an empty string', async () => {
+            const res = await exec().send({ title: '' });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if title is more than 250 characters', async () => {
+            const title = new Array(252).join('a');
+            const res = await exec().send({ title });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if description is not a string', async () => {
+            const res = await exec().send({ description: true });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if description is an empty string', async () => {
+            const res = await exec().send({ description: '' });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if startedDateTime is not a string', async () => {
+            const res = await exec().send({ startedDateTime: true });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if startedDateTime is an empty string', async () => {
+            const res = await exec().send({ startedDateTime: '' });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if startedDateTime is not a valid IOS date', async () => {
+            const res = await exec().send({
+                startedDateTime: '2020-20-20 10:00'
+            });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if startedDateTime is before now', async () => {
+            const res = await exec().send({
+                startedDateTime: '2020-02-02 10:00'
+            });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if endedDateTime is not a string', async () => {
+            const res = await exec().send({ endedDateTime: true });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if endedDateTime is an empty string', async () => {
+            const res = await exec().send({ endedDateTime: '' });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if endedDateTime is not a valid IOS date', async () => {
+            const res = await exec().send({
+                endedDateTime: '2020-20-20 10:00'
+            });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if endedDateTime is before now', async () => {
+            const res = await exec().send({
+                endedDateTime: '2020-02-02 10:00'
+            });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if startedDateTime is after endedDateTime', async () => {
+            const res = await exec().send({
+                startedDateTime: '2023-10-10 10:00',
+                endedDateTime: '2023-10-10 09:00'
+            });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if input startedDateTime is greater than the endedDateTime', async () => {
+            const res = await exec().send({
+                startedDateTime: '2024-12-12 10:00'
+            });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if input endedDateTime is less than the startedDateTime', async () => {
+            const res = await exec().send({
+                endedDateTime: '2022-10-10 10:00'
+            });
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 200, and update the announcement if request is valid', async () => {
+            const res = await exec().send({
+                title: 'qwe',
+                description: 'qweqwe',
+                startedDateTime: '2023-01-23 10:10',
+                endedDateTime: '2024-01-23 10:10'
+            });
+
+            const announcementInDb = await Announcement.findById(
+                announcementId
+            );
+
+            expect(res.status).toBe(200);
+            expect(announcementInDb.title).toBe('qwe');
+            expect(announcementInDb.description).toBe('qweqwe');
+            expect(
+                new Date(announcementInDb.startedDateTime).toISOString()
+            ).toBe(new Date('2023-01-23 10:10').toISOString());
+            expect(new Date(announcementInDb.endedDateTime).toISOString()).toBe(
+                new Date('2024-01-23 10:10').toISOString()
+            );
+            expect(announcementInDb.updatedAt).not.toBeNull();
+        });
+
+        it('should return 200, and return the updated announcement if request is valid', async () => {
+            const res = await exec().send({
+                title: 'qwe',
+                description: 'qweqwe',
+                startedDateTime: '2023-01-23 10:10',
+                endedDateTime: '2024-01-23 10:10'
+            });
+
+            expect(res.status).toBe(200);
+            expect(res.body.data).toHaveProperty(
+                '_id',
+                announcementId.toHexString()
+            );
+            expect(res.body.data).toHaveProperty('title', 'qwe');
+            expect(res.body.data).toHaveProperty('description', 'qweqwe');
+            expect(res.body.data).toHaveProperty('startedDateTime');
+            expect(res.body.data).toHaveProperty('endedDateTime');
+            expect(res.body.data).toHaveProperty('updatedAt');
+        });
+    });
 });
